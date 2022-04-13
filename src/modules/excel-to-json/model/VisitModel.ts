b691094591056942a20dc553c2fe4ba4fa77991f
Jose Carlos Santanna Junior <jcsj2010@gmail.com>
2022-04-13 17:50:34 -0300

    13.04.22
    Start to develop module visitor

---
+++ src/modules/excel-to-json/model/VisitModel.ts
@@ -0,0 +1,102 @@
+export class VisitModel {
+  ID: string;
+  DET_NOME: string;
+  DET_MATRICULA: string;
+  DET_DIGITO: string;
+  MATRICULA_DIGITO: string;
+  INC_PROCEDENCIA: string;
+  INC_RAIO: string;
+  INC_CELA: string;
+  LOCALIZACAO: string;
+  VIS_ID: string;
+  VIS_NOME: string;
+  VIS_PARLATORIO: string;
+  VIS_RG: string;
+  VIS_CPF: string;
+  DVI_PARENTESCO: string;
+  VIS_DATANASCIMENTO: string;
+  VIS_SEXO: string;
+  VIS_VENCIMENTO: string;
+  VIS_ENDERECO_VENC: string;
+  VIS_ENDERECO: string;
+  VIS_CIDADE_ID: string;
+  ENDERECO_CIDADE: string;
+  ENDERECO_UF: string;
+  VIS_BLOQUEADO: string;
+  VIS_DATACADASTRO: string;
+  VIS_DATAALTERACAO: string;
+  DVI_STATUS: string;
+  VIS_OBS: string;
+  VIS_OBS_DISPONIVEL: string;
+  MOVIMENTACAO_DATA: string;
+  MOVIMENTACAO_MOTIVO: string;
+  IDADE: string;
+
+  constructor(
+    ID: string,
+    DET_NOME: string,
+    DET_MATRICULA: string,
+    DET_DIGITO: string,
+    MATRICULA_DIGITO: string,
+    INC_PROCEDENCIA: string,
+    INC_RAIO: string,
+    INC_CELA: string,
+    LOCALIZACAO: string,
+    VIS_ID: string,
+    VIS_NOME: string,
+    VIS_PARLATORIO: string,
+    VIS_RG: string,
+    VIS_CPF: string,
+    DVI_PARENTESCO: string,
+    VIS_DATANASCIMENTO: string,
+    VIS_SEXO: string,
+    VIS_VENCIMENTO: string,
+    VIS_ENDERECO_VENC: string,
+    VIS_ENDERECO: string,
+    VIS_CIDADE_ID: string,
+    ENDERECO_CIDADE: string,
+    ENDERECO_UF: string,
+    VIS_BLOQUEADO: string,
+    VIS_DATACADASTRO: string,
+    VIS_DATAALTERACAO: string,
+    DVI_STATUS: string,
+    VIS_OBS: string,
+    VIS_OBS_DISPONIVEL: string,
+    MOVIMENTACAO_DATA: string,
+    MOVIMENTACAO_MOTIVO: string,
+    IDADE: string,
+  ) {
+    this.ID = ID;
+    this.DET_NOME = DET_NOME;
+    this.DET_MATRICULA = DET_MATRICULA;
+    this.DET_DIGITO = DET_DIGITO;
+    this.MATRICULA_DIGITO = MATRICULA_DIGITO;
+    this.INC_PROCEDENCIA = INC_PROCEDENCIA;
+    this.INC_RAIO = INC_RAIO;
+    this.INC_CELA = INC_CELA;
+    this.LOCALIZACAO = LOCALIZACAO;
+    this.VIS_ID = VIS_ID;
+    this.VIS_NOME = VIS_NOME;
+    this.VIS_PARLATORIO = VIS_PARLATORIO;
+    this.VIS_RG = VIS_RG;
+    this.VIS_CPF = VIS_CPF;
+    this.DVI_PARENTESCO = DVI_PARENTESCO;
+    this.VIS_DATANASCIMENTO = VIS_DATANASCIMENTO;
+    this.VIS_SEXO = VIS_SEXO;
+    this.VIS_VENCIMENTO = VIS_VENCIMENTO;
+    this.VIS_ENDERECO_VENC = VIS_ENDERECO_VENC;
+    this.VIS_ENDERECO = VIS_ENDERECO;
+    this.VIS_CIDADE_ID = VIS_CIDADE_ID;
+    this.ENDERECO_CIDADE = ENDERECO_CIDADE;
+    this.ENDERECO_UF = ENDERECO_UF;
+    this.VIS_BLOQUEADO = VIS_BLOQUEADO;
+    this.VIS_DATACADASTRO = VIS_DATACADASTRO;
+    this.VIS_DATAALTERACAO = VIS_DATAALTERACAO;
+    this.DVI_STATUS = DVI_STATUS;
+    this.VIS_OBS = VIS_OBS;
+    this.VIS_OBS_DISPONIVEL = VIS_OBS_DISPONIVEL;
+    this.MOVIMENTACAO_DATA = MOVIMENTACAO_DATA;
+    this.MOVIMENTACAO_MOTIVO = MOVIMENTACAO_MOTIVO;
+    this.IDADE = IDADE;
+  }
+}
